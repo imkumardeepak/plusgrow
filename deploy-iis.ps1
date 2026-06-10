1478f4b4f0675c19a25ed23b683ecbcb7c699852
imkumardeepak <deepakraznish@gmail.com>
2026-06-10 12:13:11 +0530

    chore: remove redundant NODE_ENV production assignment in IIS deployment script

--- deploy-iis.ps1
+++ deploy-iis.ps1
@@ -70,7 +70,6 @@ if (Test-Path $FrontendDir) {
     
     # Force Path injection for Node.js just in case Jenkins missed it
     $env:PATH = "D:\Program Files\nodejs;C:\Program Files\nodejs;" + $env:PATH
-    $env:NODE_ENV = "production"
     
     Write-Host "Installing NPM dependencies..."
     npm.cmd install
