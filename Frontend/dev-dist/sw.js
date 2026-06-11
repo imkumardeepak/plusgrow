a22b5c304eca61b5f1ef66b925cc789b7e8cb9b5
imkumardeepak <deepakraznish@gmail.com>
2026-06-11 13:01:16 +0530

    chore: initialize MCP configuration for Jetro and add service worker implementation

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.o4p4ti391o"
+    "revision": "0.029eg3se54g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
