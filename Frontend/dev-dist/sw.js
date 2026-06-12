1db2ba706529862eaf46913a2441d5351cceccac
imkumardeepak <deepakraznish@gmail.com>
2026-06-12 17:49:45 +0530

    location

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.u3j0heot2is"
+    "revision": "0.qjutl8i8j94"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
