3b90922379e5c64d2d52c1f5fb39bc344977c351
imkumardeepak <deepakraznish@gmail.com>
2026-06-24 10:07:17 +0530

    feat: add service worker for PWA support and configure backend launch settings

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.589amhd1v1o"
+    "revision": "0.f07v69014oo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
